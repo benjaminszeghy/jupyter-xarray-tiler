09bd543dbea4765006df215deb40134ba12c6889
Matt Fisher <3608264+mfisher87@users.noreply.github.com>
2026-02-11 16:53:24 -0700

    Extract fixtures to tests.fixtures subpackage (as pytest "plugins") (#33)

--- conftest.py
+++ conftest.py
@@ -1,8 +1,15 @@
-import pytest
+from pathlib import Path
 
-pytest_plugins = ("pytest_jupyter.jupyter_server",)
+# Discover all modules in the tests.fixtures subpackage
+FIXTURES_MODULE = ["jupyter_xarray_tiler", "tests", "fixtures"]
+fixtures_dir = Path(__file__).parent.joinpath(*FIXTURES_MODULE)
+fixture_plugins = [
+    f"{'.'.join(FIXTURES_MODULE)}.{path.stem}"
+    for path in fixtures_dir.glob("*.py")
+    if not path.stem.startswith("_")
+]
 
-
-@pytest.fixture
-def jp_server_config(jp_server_config) -> dict:
-    return {"ServerApp": {"jpserver_extensions": {"jupyter_xarray_tiler": True}}}
+pytest_plugins = (
+    "pytest_jupyter.jupyter_server",
+    *fixture_plugins,
+)
